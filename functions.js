8227ad28a51d0634974bace1210193bd52c0b766
Pranjal Sharma <prnshr@amazon.com>
2021-06-10 13:20:28 +0530

    This is the final commit

--- functions.js
+++ functions.js
@@ -16,4 +16,26 @@ let sub = function sub(a,b){
     console.log(a-b);
 }
 
-sub(20,10);
+// sub(20,10);
+
+// 3... IIFE :- Immediately Invoked function Expression
+
+;(function (){
+    console.log("I am IIFE");
+}());
+
+// ;(function abs() { console.log("Welcome to GFG!"); })();
+
+
+// Arrow Functions
+
+let res = (a,b) =>{
+    return a +b;
+    // console.log("Yahoo");
+};
+
+console.log(res(10,20));
+
+
+let res1 = a => console.log(a);
+res1("Hello");
