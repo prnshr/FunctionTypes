e3952342a1835817c425a1d494ec1710c8518096
Pranjal Sharma <prnshr@amazon.com>
2021-06-10 13:01:10 +0530

    First Commit

---
+++ functions.js
@@ -0,0 +1,13 @@
+// Types of Functions:-
+
+// 1... Normal Functions:=
+function add(a,b){
+    return a+b;
+}
+
+// let res = add(10,20);
+// console.log(`The result ${res} of the whole calculation from the add function`);
+
+
+
+// 2... Function Expression
